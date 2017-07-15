ae7ab465ba740b94327677bd05959af117ded388
Anders Ha <anders@andersha.com>
2017-07-15 16:56:15 +0800

    Added CwlSignal.

--- TestingFRPTests/TestingFRPTests.swift
+++ TestingFRPTests/TestingFRPTests.swift
@@ -3,6 +3,7 @@ import enum Result.NoError
 import ReactiveSwift
 import RxSwift
 import ReactiveKit
+import CwlSignal
 @testable import TestingFRP
 
 class TestingFRPTests: XCTestCase {
@@ -49,6 +50,21 @@ class TestingFRPTests: XCTestCase {
             }
 
             _ = signal.observeNext(with: { counter += $0 })
+        }
+    }
+
+    func test_measure_CwlSignal_1() {
+        measure {
+            var counter : Int = 0
+            let signal = CwlSignal.Signal<Int>.generate { input in
+                if let input = input {
+                    for i in 1..<100_000 {
+                        input.send(value: i)
+                    }
+                }
+            }
+
+            _ = signal.subscribeValues { counter += $0 }
         }
     }
 
@@ -99,6 +115,28 @@ class TestingFRPTests: XCTestCase {
         }
     }
 
+    func test_measure_CwlSignal_2() {
+        // CwlSignal does not have a `Property` entity, so we use its `replayLast`
+        // counterpart.
+        measure {
+            var endpoints: [AnyObject] = []
+            var counter : Int = 0
+            let (input, output) = CwlSignal.Signal<Int>.create { $0.continuous() }
+
+            let multicasted = output.multicast()
+            for _ in 1..<100 {
+                let e = multicasted.subscribeValues { counter += $0 }
+                endpoints.append(e)
+            }
+
+            withExtendedLifetime(endpoints) {
+                for i in 1..<100_000 {
+                    input.send(value: i)
+                }
+            }
+        }
+    }
+
     /// ===============================================================
 
     func test_measure_ReactiveSwift_3() {
@@ -106,7 +144,7 @@ class TestingFRPTests: XCTestCase {
             let (signal, observer) = ReactiveSwift.Signal<Int, NoError>.pipe()
 
             for _ in 1..<100 {
-                signal.filter{ $0%2 == 0}.map(String.init).observeValues { _ in }
+                signal.filterMap { $0 % 2 == 0 ? String($0) : nil }.observeValues { _ in }
             }
 
             for i in 1..<100_000 {
@@ -123,13 +161,31 @@ class TestingFRPTests: XCTestCase {
             for _ in 1..<100 {
                 _ = o.filter{ $0%2 == 0}.map(String.init).subscribe(onNext: { _ in })
             }
-            
+
             for i in 1..<100_000 {
                 variable.onNext(i)
             }
         }
     }
 
+    func test_measure_CwlSignal_3() {
+        measure {
+            var endpoints: [AnyObject] = []
+            let (input, output) = CwlSignal.Signal<Int>.create()
+            let multicasted = output.multicast()
+
+            for _ in 1..<100 {
+                let e = multicasted.filterMap { $0 % 2 == 0 ? String($0) : nil }.subscribeValues { _ in }
+                endpoints.append(e)
+            }
+
+            withExtendedLifetime(endpoints) {
+                for i in 1..<100_000 {
+                    input.send(value: i)
+                }
+            }
+        }
+    }
 
     /// ===============================================================
 
@@ -171,4 +227,26 @@ class TestingFRPTests: XCTestCase {
         }
     }
 
+    func test_measure_CwlSignal_4() {
+        measure {
+            var endpoints: [AnyObject] = []
+
+            let (input1, output1) = CwlSignal.Signal<Int>.create()
+            let (input2, output2) = CwlSignal.Signal<Int>.create()
+            let multicasted1 = output1.multicast()
+            let multicasted2 = output2.multicast()
+
+            for _ in 1..<100 {
+                let e = multicasted1.combineLatest(second: multicasted2) { $0 }.subscribeValues { _ in }
+                endpoints.append(e)
+            }
+
+            withExtendedLifetime(endpoints) {
+                for i in 1..<100_000 {
+                    input1.send(value: i)
+                    input2.send(value: i)
+                }
+            }
+        }
+    }
 }
